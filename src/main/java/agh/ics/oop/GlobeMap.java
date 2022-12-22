a85df191aeb3c8f356278290c8c1a9b6fcf8a060
Tomasz Janik <tomaszjanikw@gmail.com>
2022-12-22 18:53:06 +0100

    Unfinished work on movement

--- src/main/java/agh/ics/oop/GlobeMap.java
+++ src/main/java/agh/ics/oop/GlobeMap.java
@@ -1,26 +1,14 @@
 package agh.ics.oop;
 
 public class GlobeMap extends AbstractWorldMap{
-    public GlobeMap() {
+    public GlobeMap(Vector2d upperBound, Vector2d lowerBound) {
+        super(upperBound, lowerBound);
     }
 
     public boolean canMoveTo(Vector2d position){
-        if (position.y() > getUpperBound().y() || position.y() < getUpperBound().y()){
-            return false;
-        }
-        return true;
-
-    }
-
-    public boolean place(Animal animal){
-
         return false;
     }
 
-    public boolean isOccupied(Vector2d position){
-
-        return false;
-    }
 
 
 }
--- src/main/java/agh/ics/oop/SimulationEngine.java
+++ src/main/java/agh/ics/oop/SimulationEngine.java
@@ -1,13 +1,64 @@
 package agh.ics.oop;
 
+import java.util.LinkedList;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class SimulationEngine {
     private IWorldMap map;
+    private final LinkedList<Animal> animals = new LinkedList<>();
 
-    public SimulationEngine(int mapVariant){
+    //remove later
+    private final Vector2d b1 = new Vector2d(0,0);
+    private final Vector2d b2 =  new Vector2d(11, 11);
+
+    public SimulationEngine(MapVariant mapVariant){
         switch (mapVariant){
-            case 1 -> this.map = new GlobeMap();
-            case 2 -> this.map = new HellishMap();
+            case GLOBE_MAP: this.map = new GlobeMap(b1, b2);
+            case HELLISH_MAP: this.map =new HellishMap(b1, b2);
         }
+
+
+
     }
 
+
+   public void run(){
+        //initialize
+        var visual = new MapVisualizer(map);
+        generateAnimals(5);
+       //phase 1 dead cleanup
+       System.out.println(map);
+
+       //phase 2 movement
+//       for (int i = 0; i< 20; i++){
+//
+//           animals.forEach((Animal::move));
+//           System.out.println(map);
+//       }
+
+
+       // phase 3 eating
+
+
+       // phase 4 breeding
+
+
+       // phase 5 grass growth
+
+    }
+
+    //to potem zmienie
+    private void generateAnimals(int n){
+        for(int i = 0; i< n; i++){
+            int x = ThreadLocalRandom.current().nextInt(0, 10);
+            int y = ThreadLocalRandom.current().nextInt(0, 10);
+            if(map.isOccupied(new Vector2d(x, y))){
+                i--;
+            }else {
+                Animal animal = new Animal( MapDirection.NORTH, new Vector2d(x,y), map, null, null);
+                map.place(animal);
+                this.animals.add(animal);
+            }
+        }
+    }
 }
